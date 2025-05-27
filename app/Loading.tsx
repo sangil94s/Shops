3e1e4ef95a86b5381554cd7b6429be2475e22c0b
sangil94s <bsc7417@gmail.com>
2025-05-27 18:59:12 +0900

    [Setting] 초기 설정 추가 중

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="w-12 h-12 border-4 border-t-red-500 rounded-full animate-spin" />
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-t-red-500" />
     </div>
   );
 }
