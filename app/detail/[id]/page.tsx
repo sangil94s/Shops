add3ee0c73cd7f65a97e8d9a7b1c15d04ac6ed0b
한상일 <hansang-il@MacBookAir.localdomain>
2025-05-29 14:36:50 +0900

    Add : Components 폴더에 이후 사용할 폴더 추가

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from 'next';
 
-
 export const metadata = {
-    title: "수정예정"
-}
+  title: '수정예정',
+};
 
-export default function page () {
-    return (
-        <>
-            <p>상세 페이지</p>
-        </>
-    )
+export default function page() {
+  return (
+    <>
+      <p>상세 페이지</p>
+    </>
+  );
 }
