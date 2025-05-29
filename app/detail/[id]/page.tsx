cc4737ad10996744685e43c953e1718eccc8fba5
한상일 <hansang-il@MacBookAir.localdomain>
2025-05-29 14:10:05 +0900

    Add : 이후 사용할 폴더 초안 추가

---
+++ app/detail/[id]/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next';
+
+
+export const metadata = {
+    title: "수정예정"
+}
+
+export default function page () {
+    return (
+        <>
+            <p>상세 페이지</p>
+        </>
+    )
+}
