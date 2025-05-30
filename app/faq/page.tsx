ba1ee5ba16e684f464810459c7c3d679c65d1759
한상일 <hansang-il@MacBookAir.localdomain>
2025-05-30 13:37:51 +0900

    Add : FAQ 등 추가 중

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,6 +1,12 @@
+import FaqList from '@/components/FAQ/FaqList';
+
 export const metadata = {
   title: 'FAQ',
 };
 export default function page() {
-  return <></>;
+  return (
+    <>
+      <FaqList />
+    </>
+  );
 }
